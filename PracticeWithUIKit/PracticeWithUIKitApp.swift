83ffb9c2c7807508d8b91280c0fdf2efac9f6c39
NERO-FAN <139479010+NERO-FAN@users.noreply.github.com>
2025-02-11 20:33:31 -0600

    Initial Commit

---
+++ PracticeWithUIKit/PracticeWithUIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeWithUIKitApp.swift
+//  PracticeWithUIKit
+//
+//  Created by David Medina on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PracticeWithUIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
